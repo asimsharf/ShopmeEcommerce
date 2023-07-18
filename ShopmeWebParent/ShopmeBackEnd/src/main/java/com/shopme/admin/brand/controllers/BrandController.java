222d5ac6c0adae14d21ed65c8f7cb8602ab0062d
fnrco mac <fnrcomac@fnrcos-MacBook-Pro.local>
2023-07-18 13:52:13 +0300

    Create a new brands

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/controllers/BrandController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/controllers/BrandController.java
@@ -9,12 +9,17 @@ import com.shopme.common.entity.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 
 @Controller
@@ -46,6 +51,19 @@ public class BrandController {
         model.addAttribute("pageTitle", "Create New Brand");
 
         return "brands/brand_form";
+    }
+
+    @GetMapping("/brands/save")
+    public String saveBrand(Brand brand, MultipartFile multipartFile, RedirectAttributes ra) throws IOException {
+        if(!multipartFile.isEmpty()){
+            String fileName = StringUtils.cleanPath(Objects.requireNonNull(multipartFile.getOriginalFilename()));
+            brand.setImage(fileName);
+
+            Brand savedBrand = brandService.save(brand);
+            String uploadDir = "brand-image/" + savedBrand.getId();
+
+        }
+        return "redirect:/brands";
     }
 
 }
